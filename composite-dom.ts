08b93843ccd6c4772bd54bf8e3a13d6a66f44988
Jennifer Yoakum <yoakumjm@uw.edu>
2017-01-25 11:22:31 -0800

    lab test

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,33 @@ interface IDomElement {
   print();
 }
 
+class DomElement implements IDomElement {
+  private elementName:string;
+  private childElements:DomElement[];
+  private elementTextContent:string;
+
+  constructor(elementName:string, elementTextContent?:string) {
+    this.elementName = elementName;
+    this.elementTextContent = elementTextContent;
+    this.childElements = [];
+  }
+
+  addChildElement(element:DomElement) {
+    this.childElements.push(element);
+  }
+
+  print() {
+    console.log(`<${this.elementName}>`); //print open tag
+    if (this.elementTextContent) {
+      console.log(`${this.elementTextContent}`)
+    }
+    for (let child in this.childElements) {
+      child.elementName.print();
+    }
+    console.log(`</${this.elementName}>`); //print closing tag
+  }
+}
+
+class TextNode implements IDomElement {
+
+}
